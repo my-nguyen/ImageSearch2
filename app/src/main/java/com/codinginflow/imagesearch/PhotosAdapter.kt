e1145e8901e9478c8513e04cd130224d943f48d8
Mỹ Nguyễn <mynguyen@Mys-MacBook-Pro.local>
2023-03-11 15:59:55 -0800

    p7: PagingDataAdapter

---
+++ app/src/main/java/com/codinginflow/imagesearch/PhotosAdapter.kt
@@ -0,0 +1,45 @@
+package com.codinginflow.imagesearch
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.paging.PagingDataAdapter
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.bumptech.glide.load.resource.drawable.DrawableTransitionOptions
+import com.codinginflow.imagesearch.databinding.ItemUnsplashPhotoBinding
+
+private val COMPARATOR = object : DiffUtil.ItemCallback<UnsplashData.Result>() {
+    override fun areItemsTheSame(oldItem: UnsplashData.Result, newItem: UnsplashData.Result) = oldItem.id == newItem.id
+
+    override fun areContentsTheSame(oldItem: UnsplashData.Result, newItem: UnsplashData.Result) = oldItem == newItem
+}
+
+class PhotosAdapter: PagingDataAdapter<UnsplashData.Result, PhotosAdapter.ViewHolder>(COMPARATOR) {
+    inner class ViewHolder(val binding: ItemUnsplashPhotoBinding): RecyclerView.ViewHolder(binding.root) {
+        fun bind(photo: UnsplashData.Result) {
+            binding.apply {
+                Glide.with(itemView)
+                    .load(photo.urls.regular)
+                    .centerCrop()
+                    .transition(DrawableTransitionOptions.withCrossFade())
+                    .error(R.drawable.ic_error)
+                    .into(image)
+                username.text = photo.user.username
+            }
+        }
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        val item = getItem(position)
+        if (item != null) {
+            holder.bind(item)
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val inflater = LayoutInflater.from(parent.context)
+        val binding = ItemUnsplashPhotoBinding.inflate(inflater, parent, false)
+        return ViewHolder(binding)
+    }
+}
