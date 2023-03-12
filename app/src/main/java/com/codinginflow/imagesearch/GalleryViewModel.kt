f2f60c33894c41fd1abc068b22bbfbd8beb3a492
Mỹ Nguyễn <mynguyen@Mys-MacBook-Pro.local>
2023-03-11 17:58:34 -0800

    p13: Handling Process Death

--- app/src/main/java/com/codinginflow/imagesearch/GalleryViewModel.kt
+++ app/src/main/java/com/codinginflow/imagesearch/GalleryViewModel.kt
@@ -1,6 +1,6 @@
 package com.codinginflow.imagesearch
 
-import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.SavedStateHandle
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.switchMap
 import androidx.lifecycle.viewModelScope
@@ -9,10 +9,14 @@ import dagger.hilt.android.lifecycle.HiltViewModel
 import javax.inject.Inject
 
 private const val DEFAULT_QUERY = "cats"
+private const val CURRENT_QUERY = "current"
 
 @HiltViewModel
-class GalleryViewModel @Inject constructor(val repository: Repository) : ViewModel() {
-    private val currentQuery = MutableLiveData(DEFAULT_QUERY)
+class GalleryViewModel @Inject constructor(
+    val repository: Repository,
+    state: SavedStateHandle
+) : ViewModel() {
+    private val currentQuery = state.getLiveData(CURRENT_QUERY, DEFAULT_QUERY)
     val photos = currentQuery.switchMap { query ->
         repository.searchPhotos(query).cachedIn(viewModelScope)
     }
