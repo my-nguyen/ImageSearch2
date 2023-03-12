bb282ca1ca8d1ed25d9074ec4dc4e9422d2dcaca
Mỹ Nguyễn <mynguyen@Mys-MacBook-Pro.local>
2023-03-11 17:01:38 -0800

    p12: Implementing the DetailsFragment

--- app/src/main/java/com/codinginflow/imagesearch/DetailFragment.kt
+++ app/src/main/java/com/codinginflow/imagesearch/DetailFragment.kt
@@ -1,6 +1,57 @@
 package com.codinginflow.imagesearch
 
+import android.content.Intent
+import android.graphics.drawable.Drawable
+import android.net.Uri
+import android.os.Bundle
+import android.view.View
+import androidx.core.view.isVisible
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.navArgs
+import com.bumptech.glide.Glide
+import com.bumptech.glide.load.DataSource
+import com.bumptech.glide.load.engine.GlideException
+import com.bumptech.glide.request.RequestListener
+import com.bumptech.glide.request.target.Target
+import com.codinginflow.imagesearch.databinding.FragmentDetailBinding
 
 class DetailFragment: Fragment(R.layout.fragment_detail) {
+    private val args by navArgs<DetailFragmentArgs>()
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val photo = args.photo as UnsplashData.Result
+        val binding = FragmentDetailBinding.bind(view)
+        binding.apply {
+            // calling with(view) here would be problematic and less efficient
+            Glide.with(this@DetailFragment)
+                .load(photo.urls.full)
+                .error(R.drawable.ic_error)
+                .listener(object : RequestListener<Drawable> {
+                    override fun onLoadFailed(e: GlideException?, model: Any?, target: Target<Drawable>?, isFirstResource: Boolean): Boolean {
+                        progress.isVisible = false
+                        return false
+                    }
+
+                    override fun onResourceReady(resource: Drawable?, model: Any?, target: Target<Drawable>?, dataSource: DataSource?, isFirstResource: Boolean): Boolean {
+                        progress.isVisible = false
+                        creator.isVisible = true
+                        description.isVisible = photo.description != null
+                        return false
+                    }
+                })
+                .into(image)
+            description.text = photo.description
+
+            val uri = Uri.parse(photo.user.attributionUrl)
+            val intent = Intent(Intent.ACTION_VIEW, uri)
+            creator.apply {
+                text = "Photo by ${photo.user.name} on Unsplash"
+                setOnClickListener {
+                    context.startActivity(intent)
+                }
+                paint.isUnderlineText = true
+            }
+        }
+    }
 }
--- app/src/main/java/com/codinginflow/imagesearch/MainActivity.kt
+++ app/src/main/java/com/codinginflow/imagesearch/MainActivity.kt
@@ -1,13 +1,26 @@
 package com.codinginflow.imagesearch
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
+    private lateinit var navController: NavController
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        navController = navHostFragment.navController
+        val appBarConfiguration = AppBarConfiguration(navController.graph)
+        setupActionBarWithNavController(navController, appBarConfiguration)
     }
+
+    override fun onSupportNavigateUp() = navController.navigateUp() || super.onSupportNavigateUp()
 }
